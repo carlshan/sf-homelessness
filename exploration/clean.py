2f8e0b18caef29e2c630654ce72a48e586328d6a
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-10-31 14:48:47 -0700

    more work on determining families in HMIS, plus work on deduplicating and joining CP data

--- exploration/clean.py
+++ exploration/clean.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import dateutil
 
 def get_program():
     program = pd.read_csv('../data/program with family.csv')
@@ -8,9 +10,17 @@ def get_program():
     program = program.merge(client_de_identified , on='Subject Unique Identifier')
 
     # convert dates
-    program['Converted Program Start Date'] = pd.to_datetime(program['Program Start Date'])
-    program['Converted Program End Date'] = pd.to_datetime(program['Program End Date'])
-    program['Converted DOB'] = pd.to_datetime(program['DOB'])
+    program['Unconverted Program Start Date'] = program['Program Start Date']
+    program['Program Start Date'] = pd.to_datetime(program['Program Start Date'])
+    program['Unconverted Program End Date'] = program['Program End Date']
+    program['Program End Date'] = pd.to_datetime(program['Program End Date'])
+    program['Unconverted DOB'] = program['DOB']
+    program['DOB'] = pd.to_datetime(program['DOB'])
+
+    # get age and child/adult status
+    program['Age Entered'] = program.apply(get_program_age_entered, axis=1)
+    program['Child?'] = program['Age Entered'] < 18
+    program['Adult?'] = ~program['Child?']
     
     # deduplicate individuals
     # we do an inner join because, since we pulled the program data after doing de-duplication,
@@ -18,5 +28,25 @@ def get_program():
     hmis_client_duplicates = pd.read_csv('../data/hmis_client_duplicates.csv')
     program = program.merge(hmis_client_duplicates, on='Subject Unique Identifier', how='inner')
     program['Deduplicated Subject Unique Identifier'] = program['Duplicate ClientID'].fillna(program['Subject Unique Identifier'])
+    program.drop('Duplicate ClientID', axis=1, inplace=True)
 
     return program
+
+def get_cp_client():
+    client = pd.read_csv("../data/cp_client.csv")
+
+    # deduplicate individuals
+    cp_client_duplicates = pd.read_csv('../data/cp_client_duplicates.csv')
+    client = client.merge(cp_client_duplicates[['clientid', 'Duplicate ClientID']], left_on='Clientid', right_on='clientid')
+    client['Deduplicated ClientID'] = client['Duplicate ClientID'].fillna(client['Clientid'])
+    client.drop(['clientid', 'Duplicate ClientID'], axis=1, inplace=True)
+
+    return client
+
+def get_program_age_entered(row):
+    start_date = row['Program Start Date']
+    dob = row['DOB']
+    if start_date is pd.NaT or dob is pd.NaT:
+        return np.NaN
+    else:
+        return dateutil.relativedelta.relativedelta(start_date, dob).years
