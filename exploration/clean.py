8e39a884267d4e09db4431325b90a8e69550173a
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-11-09 11:45:45 -0800

    use fuzzy matching for deduplication

--- exploration/clean.py
+++ exploration/clean.py
@@ -24,16 +24,16 @@ def get_program():
     program['Age Entered'] = program.apply(get_program_age_entered, axis=1)
     program['Child?'] = program['Age Entered'] < 18
     program['Adult?'] = ~program['Child?']
-    
-    ## # deduplicate individuals
-    ## #program = program_strict_deduplicate_individuals(program)
-    ## program = program_fuzzy_deduplicate_individuals(program)
 
-    ## # generate families
-    ## program = program_generate_families(program)
+    # deduplicate individuals
+    #program['Subject Unique Identifier'] = program_strict_deduplicate_individuals(program)
+    program['Subject Unique Identifier'] = program_fuzzy_deduplicate_individuals(program)
 
-    ## # generate family characteristics
-    ## program = program_generate_farmily_characteristics(program)
+    # generate families
+    program = program.merge(program_generate_families(program), left_on='Subject Unique Identifier', right_index=True, how='left')
+
+    # generate family characteristics
+    program = program_generate_family_characteristics(program)
 
     return program
 
@@ -42,9 +42,8 @@ def program_strict_deduplicate_individuals(program):
     # there are some clients who appear in program but don't appear in hmis_client_duplicates.
     hmis_client_duplicates = pd.read_csv('../data/hmis_client_duplicates.csv')
     program = program.merge(hmis_client_duplicates, on='Subject Unique Identifier', how='inner')
-    program['Raw Subject Unique Identifier'] = program['Subject Unique Identifier']
-    program['Subject Unique Identifier'] = program['Duplicate ClientID'].fillna(program['Subject Unique Identifier'])
-    return program.drop('Duplicate ClientID', axis=1)
+    #program['Raw Subject Unique Identifier'] = program['Subject Unique Identifier']
+    return program['Duplicate ClientID'].fillna(program['Subject Unique Identifier'])
 
 def program_fuzzy_deduplicate_individuals(program):
     # generate Deduplicated Subject Unique Identifiers by finding the min SUID for every group
@@ -52,17 +51,19 @@ def program_fuzzy_deduplicate_individuals(program):
     hmis_lp_duplicates = hmis_lp_duplicates.drop_duplicates('Subject Unique Identifier')
     hmis_lp_duplicates['Deduplicated Subject Unique Identifier'] = hmis_lp_duplicates.groupby('Set ID')['Subject Unique Identifier'].transform(min)
     # merge those Deduplicated Subject Unique Identifiers
-    program = program.merge(hmis_lp_duplicates[['Subject Unique Identifier','Deduplicated Subject Unique Identifier']], on='Subject Unique Identifier')
-    program['Raw Subject Unique Identifier'] = program['Subject Unique Identifier']
-    program['Subject Unique Identifier'] = program['Deduplicated Subject Unique Identifier'].fillna(program['Subject Unique Identifier'])
-    return program.drop('Deduplicated Subject Unique Identifier', axis=1)
+    program = program.merge(hmis_lp_duplicates[['Subject Unique Identifier','Deduplicated Subject Unique Identifier']], on='Subject Unique Identifier', how='left')
+    #program['Raw Subject Unique Identifier'] = program['Subject Unique Identifier']
+    return program['Deduplicated Subject Unique Identifier'].fillna(program['Subject Unique Identifier'])
 
 # For now, we're just using connected components: if persons A & B enter a shelter at the same time with the same
 # Family Site Identifier, then we call them connected; if persons B & C do the same, then we call them connected,
 # and thus A & C are connected as well.
-def program_generate_families(program):
-    # begin by getting relevant fields, then join it on itself to get all first-degree connections, and drop duplicates
-    families = program[['Subject Unique Identifier', 'Family Site Identifier', 'Program Start Date']].dropna()
+def program_generate_families(program, suid='Subject Unique Identifier', fid_target='Family Identifier'):
+    # begin by getting relevant fields
+    families = program[[suid, 'Family Site Identifier', 'Program Start Date']].copy()
+    # fill in individuals' FSI with their negated SUID
+    families['Family Site Identifier'] = families['Family Site Identifier'].fillna(-families[suid])
+    # join it on itself to get all first-degree connections
     families = families.set_index(['Family Site Identifier', 'Program Start Date'])
 
     # create graph
@@ -73,8 +74,7 @@ def program_generate_families(program):
     components = [f for f in nx.connected_components(G)]
 
     # create a dataframe from the conneted components, and merge it into program
-    family_identifiers = pd.DataFrame({'Family Identifier': pd.Series({cuid: idx for idx, component in enumerate(components) for cuid in component})})
-    return program.merge(family_identifiers, left_on='Subject Unique Identifier', right_index=True, how='left')
+    return pd.DataFrame({fid_target: pd.Series({cuid: idx for idx, component in enumerate(components) for cuid in component})})
 
 def program_generate_family_characteristics(program):
     program['With Child?'] = program.groupby(['Family Identifier','Program Start Date'])['Child?'].transform(any)
