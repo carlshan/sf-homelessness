64998d4d6314d2464b38a26240c7e9df822c8021
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-11-11 14:40:58 -0800

    deduplicate connecting point families

--- exploration/clean.py
+++ exploration/clean.py
@@ -31,10 +31,10 @@ def get_program():
     program['Subject Unique Identifier'] = program_fuzzy_deduplicate_individuals(program)
 
     # generate families
-    #program = program.merge(program_generate_families(program), left_on='Subject Unique Identifier', right_index=True, how='left')
+    program['Family Identifier'] = program_generate_families(program)
 
     # generate family characteristics
-    #program = program_generate_family_characteristics(program)
+    program = program_generate_family_characteristics(program)
 
     return program
 
@@ -51,26 +51,53 @@ def program_strict_deduplicate_individuals(program):
 def program_fuzzy_deduplicate_individuals(program):
     return link_plus_deduplicate_individuals(program, 'Subject Unique Identifier', '../data/hmis/hmis_client_duplicates_link_plus.csv')
 
-# For now, we're just using connected components: if persons A & B enter a shelter at the same time with the same
-# Family Site Identifier, then we call them connected; if persons B & C do the same, then we call them connected,
-# and thus A & C are connected as well.
-def program_generate_families(program, suid='Subject Unique Identifier', fid_target='Family Identifier'):
-    # begin by getting relevant fields
-    families = program[[suid, 'Family Site Identifier', 'Program Start Date']].copy()
-    # fill in individuals' FSI with their negated SUID
-    families['Family Site Identifier'] = families['Family Site Identifier'].fillna(-families[suid])
-    # join it on itself to get all first-degree connections
-    families = families.set_index(['Family Site Identifier', 'Program Start Date'])
+def program_generate_families(program, ind_id='Subject Unique Identifier', fid='Family Site Identifier'):
+    return generate_families(program, ind_id, fid, edge_fields=['Family Site Identifier', 'Program Start Date'])
 
-    # create graph
+# If persons A & B enter a shelter at the same time with the same Family Site Identifier, then we call them connected;
+# if persons B & C do the same, then we call them connected, and thus A & C are connected as well.
+def generate_families(df, ind_id, fid, edge_fields):
+    """Given a dataframe, find the family components by creating a graph, in which edges represent two individuals
+    appearing in the same program or case at the same time, and finding the connected components among the graph.
+
+    :param df: The dataframe to consider.
+    :type df: pandas.Dataframe.
+
+    :param ind_id: The column to use as the individual identifier, (e.g. 'Subject Unique Identifier' or 'Clientid').
+    :type ind_id: str.
+
+    :param fid: The field that represents the family or case (e.g. 'Family Site Identnfier' or 'Caseid').  This field
+    gets filled in for individuals in order to properly generate family IDs for them.
+    :type fid: str.
+
+    :param edge_fields: The fields of df to consider when computing edges, (e.g. ['Family Site Identifier','Program
+    Start Date'] or ['Caseid']).
+    :type edge_fields: str or [str].
+
+    """
+    # begin by getting relevant fields
+    families = df[[ind_id]+edge_fields].copy()
+    # fill in individuals' FSI with their negated SUID
+    families[fid] = families[fid].fillna(-families[ind_id])
+    families = families.set_index(edge_fields)
+
+    # join families on itself to get all first-degree connections: this is a two-column dataframe, where each row
+    # represents a pair of ind_ids that are directly connected to each other
     edges = families.merge(families, left_index=True, right_index=True)
+    # create graph
     G = nx.Graph([tuple(e) for e in edges.values])
 
     # compute connected components
     components = [f for f in nx.connected_components(G)]
 
-    # create a dataframe from the conneted components, and merge it into program
-    return pd.DataFrame({fid_target: pd.Series({cuid: idx for idx, component in enumerate(components) for cuid in component})})
+    # make bogus column name for temporary data frame that will be merged into larger dataframe.
+    target_fid = 'target_'+fid
+    # create a dataframe from the connected components: the index is the ind_id (iid) of the person, and the one column
+    # is the family of which they are a part
+    fids = pd.DataFrame({target_fid: pd.Series({iid: idx for idx, component in enumerate(components) for iid in component})})
+
+    # merge resulting dataframe with original df to index it correctly, then return the series of target_fids
+    return df.merge(fids, left_on=ind_id, right_index=True, how='left')[target_fid]
 
 def program_generate_family_characteristics(program):
     program['With Child?'] = program.groupby(['Family Identifier','Program Start Date'])['Child?'].transform(any)
@@ -97,10 +124,16 @@ def get_cp_client():
     client['Raw Clientid'] = client['Clientid']
     client['Clientid'] = client_fuzzy_deduplicate_individuals(client)
 
+    # generate families
+    client['Familyid'] = client_generate_families(client)
+
     return client
 
 def client_fuzzy_deduplicate_individuals(client):
     return link_plus_deduplicate_individuals(client, 'Clientid', '../data/connecting_point/cp_client_duplicates_link_plus.csv')
+
+def client_generate_families(client, ind_id='Clientid', fid='Caseid'):
+    return generate_families(client, ind_id, fid, edge_fields=[fid])
 
 def get_program_age_entered(row):
     start_date = row['Program Start Date']
